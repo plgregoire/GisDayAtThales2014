1b49655f186b38e6c67a04194379ba2627dc9726
plgregoire81@gmail.com <plgregoire81@gmail.com>
2014-09-20 08:57:58 -0400

    add some test data

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 
 	var viewer = new Cesium.Viewer('cesiumContainer');
+	
+	    var dataSource = new Cesium.GeoJsonDataSource();
+		viewer.dataSources.add(dataSource);
+		dataSource.loadUrl('data.geojson');
+	
 	viewer.extend(Cesium.viewerEntityMixin);
