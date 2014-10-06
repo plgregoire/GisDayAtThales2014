430227e2ab03c0fd5863c0904a7c07cb8893c870
plgregoire81@gmail.com <plgregoire81@gmail.com>
2014-10-06 18:59:02 -0400

    changed cesium api for webglearth

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-Cesium.InfoBoxViewModel.defaultSanitizer = function(rawHtml){ return rawHtml;};
-var viewer = new Cesium.Viewer('cesiumContainer');
-	
-var dataSource = new Cesium.CzmlDataSource();
-dataSource.loadUrl('data.czml');
-viewer.dataSources.add(dataSource);
-	
-viewer.extend(Cesium.viewerEntityMixin);
 
+
+
+
+function initializeCesium(){
+	Cesium.InfoBoxViewModel.defaultSanitizer = function(rawHtml){ return rawHtml;};
+	var viewer = new Cesium.Viewer('mainContainer');
+		
+	var dataSource = new Cesium.CzmlDataSource();
+	dataSource.loadUrl('data.czml');
+	viewer.dataSources.add(dataSource);
+		
+	viewer.extend(Cesium.viewerEntityMixin);
+}
+
+
+var app;
+
+    function startWE() {
+      app = new WebGLEarth('mainContainer', {
+        atmosphere: true,
+        position: [47.2, 8.5],
+        altitude: 7000000,
+        panning: true,
+        tilting: true,
+        zooming: true,
+        proxyHost: 'http://srtm.webglearth.com/cgi-bin/corsproxy.fcgi?url='
+      });
+
+    }
+
+$(function(){
+	startWE();
+});
 
