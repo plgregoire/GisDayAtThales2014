f9eba0f486d473cf6344f7ef9e9c00854a18f03e
plgregoire81@gmail.com <plgregoire81@gmail.com>
2014-10-06 21:30:28 -0400

    attempt to implement clustering : fail !

--- app.js
+++ app.js
@@ -19,20 +19,35 @@ var map;
 function startWE() {
 	options = {atmosphere: true, zoom:3};
 	map = new WE.map('mainContainer', options);
-	WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+      WE.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
+          subdomains: '1234',
+          attribution: 'Tiles Courtesy of MapQuest'
+        }).addTo(map);
 }
 	
 function initializeData(){
 	var geoJSONDataUrl = 'http://api.tiles.mapbox.com/v3/examples.map-zr0njcqy/markers.geojson';
 	$.getJSON(geoJSONDataUrl, function(data){
-		for(var i=0;i<data.features.length;i++){
-			var feature = data.features[i];
-			var marker = WE.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]).addTo(map);
+		addFeatureToLayer(data.features);
+	});
+	
+
+}
+
+function addFeatureToLayer(features){	
+		
+		for(var i=0;i<features.length;i++){
+			var feature = features[i];
+			var marker = WE.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], { title: feature.properties.title }).addTo(map);
 			marker.bindPopup(feature.properties.description, {maxWidth: 150, closeButton: true});
 			
 		}
-	});
 	
+}
+
+function addMarkerToPosition(position){
+	var videoMarker = WE.marker([position.coords.latitude, position.coords.longitude], { title: 'video' }).addTo(map);
+	$(videoMarker.element).on('click', function(){panTo(position.coords);});
 }
 
  function panTo(coords) {
@@ -42,7 +57,7 @@ function initializeData(){
 function getLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position){
-			panTo(position.coords); 
+			addMarkerToPosition(position);
 		});
 	} 
 }
